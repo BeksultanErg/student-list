f3ff0c7cc972b36a5ffca5bbd4e53111bad225b1
Beksultan <beksultan@MacBook-Pro-Beksultan.local>
2022-06-13 15:54:57 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,150 @@
+let API = "http://localhost:8000/todo";
+
+let btn = document.querySelector(".btn");
+let todoList = document.querySelector(".todo-list");
+let send = document.querySelector(".send");
+let save = document.querySelector(".save");
+let inpName = document.querySelector(".inp-name");
+let inpFirst = document.querySelector(".inp-first");
+let inpPhone = document.querySelector(".inp-phone");
+let weekKpi = document.querySelector(".week-kpi");
+let monthKpi = document.querySelector(".month-kpi");
+let mainOl = document.querySelector(".main-ol");
+let mainScreen = document.querySelector(".main-screen");
+let btnX = document.querySelector(".btn-x");
+let disabl = document.querySelector(".disabl");
+let pagination = document.querySelector(".pagination");
+btnX.addEventListener("click", function () {
+  todoList.style.display = "none";
+  mainScreen.style.display = "block";
+});
+
+send.addEventListener("click", async function () {
+  let obj = {
+    name: inpName.value,
+    inpFirst: inpFirst.value,
+    inpPhone: inpPhone.value,
+    weekKpi: weekKpi.value,
+    monthKpi: monthKpi.value,
+  };
+
+  if (
+    obj.name.trim() === "",
+    obj.inpFirst.trim() === "" ,
+    obj.inpPhone.trim() === "" ,
+    obj.weekKpi.trim() === "",
+    obj.monthKpi.trim() === ""
+  ) {
+    alert("Заполните поля!");
+  }
+
+  await fetch(API, {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+
+  todoList.style.display = "none";
+  mainScreen.style.display = "block";
+  inpName.value = "";
+  inpFirst.value = "";
+  inpPhone.value = "";
+  weekKpi.value = "";
+  monthKpi.value = "";
+  location.reload();
+  getTodoList();
+});
+
+btn.addEventListener("click", function () {
+  todoList.style.display = "flex";
+  mainScreen.style.display = "none";
+});
+
+let page = 1;
+async function getTodoList() {
+  let allTodos = await fetch(API)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+  allTodos.forEach(element => {
+    let div = document.createElement("div");
+    div.id = element.id;
+    div.style.border = "1px solid black";
+    div.style.maxWidth = "300px";
+    div.style.padding = "10px";
+    div.style.marginBottom = "10px";
+    div.style.textTransform = "Uppercase";
+    div.innerHTML = `
+    <li><strong>Иия:  </strong>   ${element.name}</li>
+    <li><strong>Фамилия:  </strong>   ${element.inpFirst}</li>
+    <li><strong>Номер телефона:  </strong>   ${element.inpPhone}</li>
+    <li><strong>Недельный KPI: </strong> ${element.weekKpi}</li>
+    <li><strong>Ежемесячный KPI: </strong> ${element.monthKpi}</li>
+    <button style="margin-top: 10px" class="btn-delete">Удалить</button>
+    <button class="btn-edit">Изменить</button>
+    `;
+    mainOl.append(div);
+  });
+}
+getTodoList();
+
+save.addEventListener("click", async function () {
+  let editedTodo = {
+    name: inpName.value,
+    inpFirst: inpFirst.value,
+    inpPhone: inpPhone.value,
+    weekKpi: weekKpi.value,
+    monthKpi: monthKpi.value,
+  };
+  let id = disabl.value;
+  await fetch(`${API}/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(editedTodo),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+  todoList.style.display = "none";
+  mainScreen.style.display = "block";
+  location.reload();
+});
+document.addEventListener("click", async function (e) {
+   if (e.target.className === "btn-delete") {
+     let id = e.target.parentNode.id;
+     await fetch(`${API}/${id}`, {
+       method: "DELETE",
+     });
+     location.reload();
+   }
+ 
+   if (e.target.className === "btn-edit") {
+     console.log(e.target);
+     todoList.style.display = "flex";
+     mainScreen.style.display = "none";
+     send.style.display = "none";
+     save.style.display = "block";
+     save.style.width = "258px";
+     save.style.height = "45px";
+     save.style.marginTop = "25px";
+     save.style.marginBottom = "25px";
+     save.style.borderRadius = "3px";
+     send.style.width = "258px";
+     send.style.height = "45px";
+     send.style.marginTop = "25px";
+     send.style.marginBottom = "25px";
+     send.style.borderRadius = "3px";
+     let id = e.target.parentNode.id;
+     console.log(id);
+     let response = await fetch(`${API}/${id}`)
+       .then(res => res.json())
+       .catch(error => console.log(error));
+     console.log(response);
+     inpName.value = response.name;
+     inpFirst.value = response.inpFirst;
+     inpPhone.value = response.inpPhone;
+     weekKpi.value = response.weekKpi;
+     monthKpi.value = response.monthKpi;
+     disabl.value = response.id;
+   }
+});
